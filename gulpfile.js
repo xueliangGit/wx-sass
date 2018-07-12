6d6e8e2ce511aabaf9ba38a5ec3a8cd48328469f
xuxueliang <xu.xueliang@163.com>
2018-07-12 16:21:05 +0800

    edit

--- gulpfile.js
+++ gulpfile.js
@@ -1,30 +1,13 @@
 // 载入外挂
 var config = require('./config')
 var gulp = require('gulp')
-// var uglify = require('gulp-uglify')
 var rename = require('gulp-rename')
-// var minifycss = require('gulp-minify-css')
-//  replace = require('gulp-replace'),
-//  watch = require('gulp-watch'),
 var notify = require('gulp-notify')
-// sourcemaps=require('gulp-sourcemaps'),
-//  less = require('gulp-less'),
 var sass = require('gulp-sass')
 var plumber = require('gulp-plumber')
 var AutoPrefix = require('gulp-autoprefixer')
-// LessPluginAutoPrefix = require('less-plugin-autoprefix');
-// var autoprefixer = LessPluginAutoPrefix({
-//     browsers:config.browsers,
-//     cascade: true
-// });
-var eslint = require('gulp-eslint')
 
-// var env = {
-// production: false
-// };
-// var reload      = browserSync.reload;
-// var spritesmith = require('gulp.spritesmith');//雪碧图
-// sass
+var eslint = require('gulp-eslint')
 gulp.task('sass', function () {
   return gulp.src(config.scss.src)
     .pipe(plumber({ errorHandler: notify.onError('Error:<%= error.message %>') }))
@@ -51,36 +34,6 @@ gulp.task('js', function () {
   // lint error, return the stream and pipe to failAfterError last.
   // .pipe(eslint.failAfterError());
 })
-// // 样式~
-// gulp.task('less',function(){
-//   if(env.production){
-//   return gulp.src(config.less.src)
-//     .pipe(plumber({errorHandler:notify.onError('Error:less--<%= error.message.split("less")[1] %>')}))
-//     .pipe(less({
-//         plugins: [autoprefixer]
-//     }))
-//    // .pipe(concat('style.css'))
-//     .pipe(gulp.dest(config.less.dist))
-//     // .pipe(rename({ suffix: '.min' }))
-//     .pipe(minifycss())
-//     .pipe(gulp.dest(config.less.dist));
-//   //  .pipe(notify({ message: 'Styles task complete' }));
-//   }else{
-//   return gulp.src(config.less.src)
-//     .pipe(plumber({errorHandler:notify.onError('Error:less--<%= error.message.split("less")[1] %>')}))
-//      .pipe(sourcemaps.init())
-//     .pipe(less({
-//         plugins: [autoprefixer]
-//     }))
-//    // .pipe(concat('style.css'))
-//     .pipe(sourcemaps.write())
-//     .pipe(gulp.dest(config.less.dist))
-//     // .pipe(rename({ suffix: '.min' }))
-//     .pipe(minifycss())
-//     .pipe(gulp.dest(config.less.dist));
-//   //  .pipe(notify({ message: 'Styles task complete' }));
-//   }
-// })
 // 开发环境
 gulp.task('dev', ['sass', 'js'], function () {
   // 看守所有.less档
